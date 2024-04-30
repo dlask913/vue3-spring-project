30cb929668a2a9f68a64511d0c9d8ddcfb7cc0e4
dlask913 <dlask913@inu.ac.kr>
2024-05-01 00:41:54 +0900

    feat: router-view 설정 및 Navbar 추가

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Navbar />
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+export default {
+    components: {
+        Navbar
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" :to="{name: 'Home'}">Home</router-link>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">                
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">게시판</a>
+                    </li>
+                    <li class="nav-item">
+                    <router-link class="nav-link" href="#">회원가입</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
