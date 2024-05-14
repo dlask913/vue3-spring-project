2809837f381e3648e0618c5103d5e5f249530316
dlask913 <dlask913@inu.ac.kr>
2024-05-15 00:20:49 +0900

    feat: Footer Component 구현

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,15 +4,18 @@
     <router-view />
   </div>
   <Toast />
+  <Footer />  
 </template>
 
 <script>
 import Navbar from '@/components/common/Navbar.vue';
 import Toast from '@/components/common/Toast.vue';
+import Footer from '@/components/common/Footer.vue';
 export default {
     components: {
         Navbar,
         Toast,
+        Footer,
     }
 }
 </script>
---
+++ frontend/src/components/common/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="bg-light mt-5 py-3 fixed-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 text-start">
+                    <p>© 2024 Limnj Inc.</p>
+                </div>
+                <div class="col-sm-6 text-end">
+                    <p>spring boot & vue3 toy project</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+</style>
