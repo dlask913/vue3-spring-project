6f428a731c527c22f53764c83da33089a9a23f5e
dlask913 <dlask913@inu.ac.kr>
2024-05-19 16:49:57 +0900

    feat: NoticeDetails Component 내 삭제 버튼 추가

--- frontend/src/components/notice/NoticeDetails.vue
+++ frontend/src/components/notice/NoticeDetails.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="container mt-5">
-    <h1 class="mb-3">{{ notice.title }}</h1>
-    <div class="mb-1">
-      <strong>작성자:</strong> {{ notice.username }}
+  <div class="container mt-5" style="width: 60%;">
+        <div class="card">
+            <div class="card-header">
+                <h2>{{ notice.title }}</h2>
+            </div>
+            <div class="card-body">
+                <h6 class="card-subtitle mb-2 text-muted">
+                    <strong>작성자:</strong> {{ notice.username }} | <strong>작성일:</strong> {{ notice.postDate }}
+                </h6>
+                <p class="card-text mt-2" style="min-height: 300px;">{{ notice.content }}</p>
+            </div>
+            
+        </div>
+        <div class="d-flex justify-content-end mt-4">
+            <button class="btn btn-secondary me-2" @click="$router.push('/post')">뒤로 가기</button>
+            <button v-if="isEditable" class="btn btn-primary me-2" @click="moveToPage">수정</button>
+            <button v-if="isEditable" class="btn btn-danger" @click="onDelete">삭제</button>
+        </div>
     </div>
-    <div class="mb-1">
-      <strong>작성일:</strong> {{ notice.postDate }}
-    </div>
-    <div class="mt-3 mb-3">
-      <p>{{ notice.content }}</p>
-    </div>
-    <button class="btn btn-secondary me-3" @click="$router.push('/post')">뒤로 가기</button>
-    <button v-if="isEditable" class="btn btn-primary" @click="moveToPage()">수정하기</button>
-  </div>
 </template>
 <script>
 import axios from '@/axios';
 import { useRoute, useRouter } from 'vue-router';
 import { ref, computed } from 'vue';
-import { useStorageStore } from '@/store/index';
+import { useStorageStore, useToastStore } from '@/store/index';
 export default {
   setup(){
     const route = useRoute();
     const router = useRouter();
     const storage = useStorageStore();
+    const toast = useToastStore();
     const notice = ref({
       title: '',
       content: '',
@@ -49,12 +55,25 @@ export default {
       router.push('/post/' + noticeId);
     };
 
+    const onDelete = async () => {
+      try{
+        let res = await axios.delete(`notice/${noticeId}`,{
+          headers: {'Authorization': storage.getToken, }
+        });
+        router.push('/post');
+        toast.setToast('게시글이 삭제되었습니다.');
+      } catch(error){
+        toast.setToast('게시글 삭제에 실패하였습니다.','danger');
+      }
+    };
+
     getNotice();
 
     return {
       notice,
       isEditable,
       moveToPage,
+      onDelete,
     }
   }
 }
