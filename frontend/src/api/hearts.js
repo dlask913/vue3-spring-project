6dee56aed3c8eaa61dc1e39bd0e3c425d207f357
dlask913 <dlask913@inu.ac.kr>
2024-06-01 15:58:32 +0900

    feat: 좋아요 API 요청 메서드 생성

---
+++ frontend/src/api/hearts.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export function getHeartStatus(data) {
+  return axios.get(`http://localhost:8080/heart`, data);
+}
+
+export function saveHeart(token, data) {
+  return axios.post(`http://localhost:8080/heart`, data,
+    {
+      headers: {'Authorization': token }
+    });
+}
+
+export function removeHeart(token, id) {
+  return axios.delete(`http://localhost:8080/heart/${id}`,
+    {
+      headers: {'Authorization': token }
+    });
+}
