48898a5e4dfae7845eb818fff75b63740116e01a
dlask913 <dlask913@inu.ac.kr>
2024-08-05 21:56:46 +0900

    feat: 이미지 리소스 핸들러 설정 추가

---
+++ backend/src/main/java/com/example/noticeboardservice/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.example.noticeboardservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${uploadPath}")
+    String uploadPath;
+    @Value("${defaultImgPath}")
+    String defaultImgPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**") // 해당 url로 시작하는 경우
+            .addResourceLocations(uploadPath); // uploadPath 에 설정한 폴더를 기준으로 파일을 읽어옴
+
+        registry.addResourceHandler("/image/**") // default img 접속 경로
+            .addResourceLocations(defaultImgPath); // defaultImgPath 에 설정한 폴더를 기준으로 파일을 읽어옴
+
+    }
+}
