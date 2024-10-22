06df9f46ba95477262b1f1fd15f68f64707226e9
dlask913 <dlask913@inu.ac.kr>
2024-10-22 12:18:25 +0900

    feat: FE 상품 등록 시 이미지 업로드 기능 추가

--- frontend/src/components/product/ProductList.vue
+++ frontend/src/components/product/ProductList.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="row row-cols-1 row-cols-md-4 g-4 ms-2">
-    <div class="col" v-for="product in products" :key="product.id">
-      <div class="card">
-        <img
-          :src="'http://localhost:8080' + product.imgUrl"
-          class="card-img-top"
-          alt="Preview"
-        />
-        <div class="card-body">
-          <h5 class="card-title">{{ product.title }}</h5>
-          <p class="card-text">
-            {{ product.content }}
-          </p>
-          <h4>{{ product.standardPrice }} 원</h4>
-          <p class="card-text">
-            <small class="text-muted">Created at {{ product.postDate }}</small>
-          </p>
+  <div class="container mt-5" style="width: 70%">
+    <div class="row row-cols-1 row-cols-md-4 g-4 ms-2">
+      <div class="col" v-for="product in products" :key="product.id">
+        <div class="card">
+          <img
+            :src="'http://localhost:8080' + product.imgUrl"
+            class="card-img-top"
+            alt="Preview"
+          />
+          <div class="card-body">
+            <h5 class="card-title">{{ product.title }}</h5>
+            <p class="card-text">
+              {{ product.content }}
+            </p>
+            <h4>{{ product.standardPrice }} 원</h4>
+            <p class="card-text">
+              <small class="text-muted"
+                >Created at {{ product.postDate }}</small
+              >
+            </p>
+          </div>
         </div>
       </div>
     </div>
