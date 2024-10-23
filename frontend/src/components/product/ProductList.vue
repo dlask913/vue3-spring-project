dad09ff60534bc5cc87e9fe74605597c97cc07a3
dlask913 <dlask913@inu.ac.kr>
2024-10-23 22:09:31 +0900

    feat: FE ProductList 내 Category 조회 옵션 UI 추가 (#7)

--- frontend/src/components/product/ProductList.vue
+++ frontend/src/components/product/ProductList.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="container mt-5" style="width: 70%">
+    <h1 class="mb-4">상품 리스트</h1>
+
+    <!-- 카테고리별 상품 보기 -->
+    <div class="category-container">
+      <div class="category" v-for="category in categories" :key="category.name">
+        <img :src="category.image" :alt="category.name" class="category-image" />
+        <p>{{ category.name }}</p>
+      </div>
+    </div>
+
+    <hr />
     <div class="row row-cols-1 row-cols-md-4 g-4 ms-2">
       <div class="col" v-for="product in products" :key="product.id">
         <div class="card">
@@ -31,6 +42,16 @@ import { ref } from 'vue';
 
 const products = ref({});
 
+const categories = [
+        { name: '가구', image: 'http://localhost:8080/image/productDefaultImg.jpg' },
+        { name: '침구', image: 'http://localhost:8080/image/productDefaultImg.jpg' },
+        { name: '패브릭', image: 'http://localhost:8080/image/productDefaultImg.jpg' },
+        { name: '조명', image: 'http://localhost:8080/image/productDefaultImg.jpg' },
+        { name: '가구', image: 'http://localhost:8080/image/productDefaultImg.jpg' },
+        { name: '침구', image: 'http://localhost:8080/image/productDefaultImg.jpg' },
+        // 추가 카테고리들
+      ];
+
 const fetchProducts = async () => {
   const { data } = await getProducts();
   products.value = data;
@@ -38,4 +59,34 @@ const fetchProducts = async () => {
 
 fetchProducts();
 </script>
-<style scoped></style>
+<style scoped>
+.card-img-top {
+  object-fit: cover; /* 비율을 유지하면서 크기에 맞춰 자르기 */
+}
+
+.category-container {
+  display: flex;
+  justify-content: center; /* 수평 중앙 정렬 */
+  align-items: center; /* 수직 중앙 정렬 */
+  gap: 20px; /* 요소 사이 간격 */
+  padding: 20px;
+}
+
+.category {
+  flex: 0 0 auto; /* 요소가 줄어들지 않도록 고정 */
+  text-align: center;
+}
+
+.category-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-bottom: 10px;
+}
+
+.category p {
+  font-size: 14px;
+  color: #666;
+}
+</style>
