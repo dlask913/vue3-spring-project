f953ef638e6598ea11c44051deba97715cb249eb
dlask913 <dlask913@inu.ac.kr>
2024-12-03 12:35:27 +0900

    chore: ESLint 및 Prettier 설정 재구성 및 npm 패키지 업데이트

---
+++ noticeboard-service/frontend/eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+    rules: {
+      'no-console': 'warn', 
+      'vue/no-undef-components': 'error', 
+    },
+  },
+
+  {
+    name: 'app/files-to-ignore', 
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  skipFormatting,
+]
