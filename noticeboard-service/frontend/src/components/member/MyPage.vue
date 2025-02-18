299ba90a21d5deb4c6e499a8d6253fbd54aedb9a
dlask913 <dlask913@inu.ac.kr>
2025-02-18 21:30:20 +0900

    feat: FE 사이드바 메뉴 동적 전환 기능 추가 및 메시지 조회 기능 구현 (#22)

--- noticeboard-service/frontend/src/components/member/MyPage.vue
+++ noticeboard-service/frontend/src/components/member/MyPage.vue
@@ -3,7 +3,7 @@
     <!-- 사이드바 -->
     <div
       class="d-flex flex-column flex-shrink-0 p-3 mt-5 ms-5"
-      style="width: 280px;"
+      style="width: 280px"
     >
       <div
         class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
@@ -13,23 +13,51 @@
       <hr />
       <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-          <a href="#" class="nav-link active" aria-current="page"> Home </a>
+          <a
+            href="#"
+            class="nav-link"
+            :class="{ active: selectedMenu === 'home' }"
+            @click.prevent="selectedMenu = 'home'"
+          >
+            Home
+          </a>
         </li>
         <li>
-          <a href="#" class="nav-link link-dark"> Posts </a>
+          <a
+            href="#"
+            class="nav-link link-dark"
+            :class="{ active: selectedMenu === 'posts' }"
+            @click.prevent="selectedMenu = 'posts'"
+          >
+            Posts
+          </a>
         </li>
         <li>
-          <a href="#" class="nav-link link-dark"> Comments </a>
+          <a
+            href="#"
+            class="nav-link link-dark"
+            :class="{ active: selectedMenu === 'comments' }"
+            @click.prevent="selectedMenu = 'comments'"
+          >
+            Comments
+          </a>
         </li>
         <li>
-          <a href="#" class="nav-link link-dark"> Messages </a>
+          <a
+            href="#"
+            class="nav-link link-dark"
+            :class="{ active: selectedMenu === 'messages' }"
+            @click.prevent="selectedMenu = 'messages'"
+          >
+            Messages
+          </a>
         </li>
       </ul>
     </div>
 
     <!-- 메인 콘텐츠 -->
     <div class="container mt-5 me-5" style="">
-      <div class="row">
+      <div class="row" v-if="selectedMenu === 'home'">
         <div class="col-md-4 text-center">
           <ImageUploader
             v-if="member.imgUrl"
@@ -75,7 +103,8 @@
         </div>
       </div>
 
-      <div class="row mt-5">
+      <!-- 내가 작성한 게시물 -->
+      <div class="row mt-5" v-if="selectedMenu === 'posts'">
         <div class="col-md-5">
           <h3>My Posts</h3>
           <div
@@ -97,6 +126,10 @@
             </div>
           </div>
         </div>
+      </div>
+
+      <!-- 내가 작성한 댓글 -->
+      <div class="row mt-5" v-if="selectedMenu === 'comments'">
         <div class="col-md-5">
           <h3>My Comments</h3>
           <div class="card mb-3" v-for="comment in comments" :key="comment.id">
@@ -119,6 +152,45 @@
           </div>
         </div>
       </div>
+
+      <!-- 내가 보낸 메시지 -->
+      <div class="row mt-5" v-if="selectedMenu === 'messages'">
+        <div class="col-md-5">
+          <h3>보낸 메시지</h3>
+          <div
+            class="card mb-3"
+            v-for="message in sentMessages"
+            :key="message.id"
+          >
+            <div class="card-body">
+              <div class="d-flex justify-content-between">
+                <p class="card-text">{{ message.content }}</p>
+              </div>
+              <p class="card-text">
+                <small class="text-muted">Sent on {{ message.postDate }}</small>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-5">
+          <h3>받은 메시지</h3>
+          <div
+            class="card mb-3"
+            v-for="message in receivedMessages"
+            :key="message.id"
+          >
+            <div class="card-body">
+              <div class="d-flex justify-content-between">
+                <p class="card-text">{{ message.content }}</p>
+              </div>
+              <p class="card-text">
+                <small class="text-muted">Sent on {{ message.postDate }}</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -129,6 +201,10 @@ import { ref, onMounted } from 'vue'
 import { getMemberById, updateMember } from '@/api/users'
 import { getNoticesByMember } from '@/api/notices'
 import { getCommentsByMember } from '@/api/comments'
+import {
+  getSentMessagesByMemberId,
+  getReceivedMessagesByMemberId,
+} from '@/api/messages'
 import { useToastStore, useStorageStore } from '@/store'
 import { useRouter } from 'vue-router'
 
@@ -144,19 +220,31 @@ const member = ref({
 const memberImg = ref(null)
 const notices = ref([])
 const comments = ref([])
+const sentMessages = ref([])
+const receivedMessages = ref([])
 const isActive = ref(null)
+const selectedMenu = ref('home') // 현재 선택된 메뉴
 
 const fetchData = async () => {
   try {
-    const [memberResponse, noticesResponse, commentsResponse] =
-      await Promise.all([
-        getMemberById(storage.getToken, storage.getUserId),
-        getNoticesByMember(storage.getToken),
-        getCommentsByMember(storage.getToken),
-      ])
+    const [
+      memberResponse,
+      noticesResponse,
+      commentsResponse,
+      sentMessagesResponse,
+      receivedMessagesResponse,
+    ] = await Promise.all([
+      getMemberById(storage.getToken, storage.getUserId),
+      getNoticesByMember(storage.getToken),
+      getCommentsByMember(storage.getToken),
+      getSentMessagesByMemberId(storage.getToken, storage.getUserId),
+      getReceivedMessagesByMemberId(storage.getToken, storage.getUserId),
+    ])
     member.value = memberResponse.data
     notices.value = noticesResponse.data
     comments.value = commentsResponse.data
+    sentMessages.value = sentMessagesResponse.data
+    receivedMessages.value = receivedMessagesResponse.data
     member.value.imgUrl = 'http://localhost:8080' + member.value.imgUrl // todo: baseUrl 따로 빼기
   } catch (e) {
     console.error(e.response.data)
