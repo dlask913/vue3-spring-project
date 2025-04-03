ecb08cdc3b3ad1bee106dfd27b16d5c8ccfef50d
dlask913 <dlask913@inu.ac.kr>
2025-04-03 23:23:38 +0900

    fix: AddressRequestDto에 id 필드 추가하여 MyBatis 자동 키 매핑 오류 해결 (#29)

--- noticeboard-service/backend/src/main/java/com/example/noticeboardservice/dto/AddressRequestDto.java
+++ noticeboard-service/backend/src/main/java/com/example/noticeboardservice/dto/AddressRequestDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class AddressRequestDto {
+    Long id;
     Long memberId;
     String addressName;
     String roadAddressName;
