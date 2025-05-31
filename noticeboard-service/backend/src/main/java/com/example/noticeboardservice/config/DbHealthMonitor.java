ec3e72e36662845a195ee524fe9afb9710ec1f2c
dlask913 <dlask913@inu.ac.kr>
2025-05-31 21:55:07 +0900

    feat: DB Health 체크 시 Slack 알림 전송 기능 추가 (#40)

--- noticeboard-service/backend/src/main/java/com/example/noticeboardservice/config/DbHealthMonitor.java
+++ noticeboard-service/backend/src/main/java/com/example/noticeboardservice/config/DbHealthMonitor.java
@@ -1,17 +1,27 @@
 package com.example.noticeboardservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.HealthComponent;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.health.Status;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DbHealthMonitor {
 
     @Autowired
     private HealthEndpoint healthEndpoint;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${slack.webhook.url}")
+    private String slackWebhookUrl;
+
 
     @Scheduled(fixedDelay = 10000) // 10초마다 확인
     public void checkDbHealth() {
@@ -27,5 +37,17 @@ public class DbHealthMonitor {
 
     private void sendAlert(String message) {
         System.out.println("[ALERT] " + message);
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            String payload = String.format("{\"text\": \"%s\"}", message);
+            HttpEntity<String> entity = new HttpEntity<>(payload, headers);
+
+            restTemplate.postForEntity(slackWebhookUrl, entity, String.class);
+        } catch (Exception e) {
+            System.err.println("[SLACK ERROR] " + e.getMessage());
+        }
     }
 }
