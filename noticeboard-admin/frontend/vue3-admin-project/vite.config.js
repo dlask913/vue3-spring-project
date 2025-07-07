701336d958b46d49e3ace84e1149e36f4845f728
dlask913 <dlask913@inu.ac.kr>
2025-07-07 22:19:02 +0900

    chore: Admin 페이지 개발을 위해 기본 템플릿 초기 파일 제거

--- noticeboard-admin/frontend/vue3-admin-project/vite.config.js
+++ noticeboard-admin/frontend/vue3-admin-project/vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    port: 5174
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
