418cfc1aef8010c7938fd00032de306ffb777c5b
dlask913 <dlask913@inu.ac.kr>
2025-07-09 23:23:20 +0900

    feat: CKEditor 설치 및 전역 등록 설정 추가 (#50)

--- noticeboard-admin/frontend/vue3-admin-project/src/main.js
+++ noticeboard-admin/frontend/vue3-admin-project/src/main.js
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import CKEditor from '@ckeditor/ckeditor5-vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(CKEditor)
 
 app.mount('#app')
