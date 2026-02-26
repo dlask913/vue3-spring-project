443ffa749e976587186ad390e211aa92fe7e7c92
dlask913 <dlask913@inu.ac.kr>
2026-02-26 21:54:11 +0900

    fix: FCM Service Worker 등록 오류 해결을 위해 compat 방식으로 변경 (#80)

--- noticeboard-admin/frontend/public/firebase-messaging-sw.js
+++ noticeboard-admin/frontend/public/firebase-messaging-sw.js
@@ -1,10 +1,14 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js';
-import {
-  getMessaging,
-  onBackgroundMessage,
-} from 'https://www.gstatic.com/firebasejs/12.9.0/firebase-messaging-sw.js';
+// eslint-disable-next-line no-undef
+importScripts(
+  'https://www.gstatic.com/firebasejs/12.9.0/firebase-app-compat.js',
+);
+// eslint-disable-next-line no-undef
+importScripts(
+  'https://www.gstatic.com/firebasejs/12.9.0/firebase-messaging-compat.js',
+);
 
-const firebaseConfig = {
+// eslint-disable-next-line no-undef
+firebase.initializeApp({
   apiKey: 'AIzaSyBis3DdEsr1ld5v5WPlHNAEbqrToa6Uqv0',
   authDomain: 'toyproject-limnj.firebaseapp.com',
   projectId: 'toyproject-limnj',
@@ -12,14 +16,13 @@ const firebaseConfig = {
   messagingSenderId: '1009234756382',
   appId: '1:1009234756382:web:add0045227a45e7356ff0f',
   measurementId: 'G-00E4XE048J',
-};
+});
 
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+// eslint-disable-next-line no-undef
+const messaging = firebase.messaging();
 
-onBackgroundMessage(messaging, payload => {
+messaging.onBackgroundMessage(function (payload) {
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: '/icons/icon-128x128.png',
   });
 });
