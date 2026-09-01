d58c5f367e1afb8d16f5c68b241a392c54a8a38d
dlask913 <dlask913@inu.ac.kr>
2026-09-01 21:24:05 +0900

    feat: service 실시간 접속자 수 카운팅을 위한 stompClient 연결 (#98)

--- noticeboard-service/frontend/src/App.vue
+++ noticeboard-service/frontend/src/App.vue
@@ -11,6 +11,29 @@
 import Navbar from '@/components/common/Navbar.vue'
 import Toast from '@/components/common/Toast.vue'
 import Footer from '@/components/common/Footer.vue'
+
+import { onMounted, onUnmounted } from 'vue'
+import { Client } from '@stomp/stompjs'
+import SockJS from 'sockjs-client'
+
+let stompClient = null
+
+onMounted(() => {
+  stompClient = new Client({
+    webSocketFactory: () => new SockJS('http://localhost:8080/ws-connection'),
+    reconnectDelay: 5000,
+  })
+  
+  // 연결만 맺어두면 백엔드 SessionConnectedEvent가 자동으로 카운트함
+  stompClient.activate()
+})
+
+onUnmounted(() => {
+  if (stompClient) {
+    stompClient.deactivate()
+  }
+})
+
 </script>
 
 <style></style>
